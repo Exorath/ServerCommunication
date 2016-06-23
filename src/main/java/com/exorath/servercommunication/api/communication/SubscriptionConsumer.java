ec035956b8659ae1ed6b045ce7614e50b4588c42
Toon Sevrin <twan123@live.be>
2016-06-23 18:22:10 +0000

    Deploy artifacts for 1.0-SNAPSHOT

--- src/main/java/com/exorath/servercommunication/api/communication/SubscriptionConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.exorath.servercommunication.api.communication;
-
-/**
- * Created by Toon Sevrin on 6/14/2016.
- */
-public interface SubscriptionConsumer {
-    void onPublish(String channel, String message);
-}
